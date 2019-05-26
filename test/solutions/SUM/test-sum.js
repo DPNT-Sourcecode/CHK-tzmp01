023d37eb84a15a651546ed93b93d65551075c7ef
sbx_user1051 <sbx_user1051@169.254.216.249>
2019-05-26 18:42:07 +0000

    Sun May 26 18:42:07 UTC 2019

--- test/solutions/SUM/test-sum.js
+++ test/solutions/SUM/test-sum.js
@@ -1,27 +1,27 @@
-var mocha = require('mocha')
-var describe = mocha.describe
-var it = mocha.it
-var assert = require('assert');
-const sum = require('../../../lib/solutions/SUM/sum');
+var mocha = require("mocha");
+var describe = mocha.describe;
+var it = mocha.it;
+var assert = require("assert");
+const sum = require("../../../lib/solutions/SUM/sum");
 
-describe('SUM challenge: adding two numbers', function() {
-	it('should return 3, which is the sum of 1 and 2', function() {
-	    assert.equal(sum(1, 2), 3);
-	});
+describe("SUM challenge: adding two numbers", function() {
+  it("should return 3, which is the sum of 1 and 2", function() {
+    assert.equal(sum(1, 2), 3);
+  });
 
-	it('should throw an error when param[0] is less than 0', function() {
-		assert.throws(sum(-1,2))
-	})
+  it("should throw an error when param[0] is less than 0", function() {
+    assert.throws(sum(-1, 2));
+  });
 
-	it('should throw an error when param[1] is less than 0', function() {
-		assert.throws(sum(1,-2))
-	})
+  it("should throw an error when param[1] is less than 0", function() {
+    assert.throws(sum(1, -2));
+  });
 
-	it('should throw an error is param[0] is greater than 100', function() {
-		asssert.throws(sum(101,2))
-	}
+  it("should throw an error is param[0] is greater than 100", function() {
+    asssert.throws(sum(101, 2));
+  });
 
-	it('should throw an error is param[1] is greater than 100', function() {
-		asssert.throws(sum(1,102))
-	}
+  it("should throw an error is param[1] is greater than 100", function() {
+    asssert.throws(sum(1, 102));
+  });
 });
