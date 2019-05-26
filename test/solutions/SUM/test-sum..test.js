72f78d6f7bad75b4b12c3cd1b69a1dadd2944ceb
sbx_user1051 <sbx_user1051@169.254.216.249>
2019-05-26 18:57:07 +0000

    Sun May 26 18:57:07 UTC 2019

--- test/solutions/SUM/test-sum..test.js
+++ test/solutions/SUM/test-sum..test.js
@@ -6,19 +6,20 @@ describe("SUM challenge: adding two numbers", function() {
   });
 
   it("should throw an error when param[0] is less than 0", function() {
-    expect(() => sum(-1, 2)).toThrow("param[0] is less than 0");
+    expect(() => sum(-1, 2)).toThrowError("param[0] is less than 0");
   });
 
   it("should throw an error when param[1] is less than 0", function() {
-    expect(() => sum(1, -2));
+    expect(() => sum(1, -2)).toThrowError("param[1] is less than 0");
   });
 
   it("should throw an error is param[0] is greater than 100", function() {
-    asssert.throws(sum(101, 2));
+    expect(() => sum(101, 2)).toThrowError("param[0] is greater than 100");
   });
 
   it("should throw an error is param[1] is greater than 100", function() {
-    asssert.throws(sum(1, 102));
+    expect(() => sum(1, 102)).toThrowError("param[1] is greater than 100");
   });
 });
+
 
