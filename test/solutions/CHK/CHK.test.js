01f94e9f81ee5feed3aa381af4ba91947d79b7b3
sbx_user1051 <sbx_user1051@169.254.216.249>
2019-05-26 19:12:59 +0000

    Sun May 26 19:12:59 UTC 2019

--- test/solutions/CHK/CHK.test.js
+++ test/solutions/CHK/CHK.test.js
@@ -1,1 +1,22 @@
 const checkout = require("../../../lib/solutions/CHK/checkout");
+
+test("passing the price for A returns the value for A", () => {
+  expect(checkout("A")).toBe(50);
+});
+
+test("passing the price for B returns the value for B", () => {
+  expect(checkout("B")).toBe(30);
+});
+
+test("passing the price for C returns the value for C", () => {
+  expect(checkout("C")).toBe(20);
+});
+
+test("passing the price for D returns the value for D", () => {
+  expect(checkout("D")).toBe(15);
+});
+
+test("passing the price for E returns the an error as E does not exist", () => {
+  expect(() => checkout("E")).toThrowError("Product doesn`t exist");
+});
+
