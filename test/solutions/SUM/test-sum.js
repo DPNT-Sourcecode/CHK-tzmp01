22d85ba6b0b52e400904e9f1d9ec43b757930757
sbx_user1051 <sbx_user1051@169.254.216.249>
2019-05-26 18:39:07 +0000

    Sun May 26 18:39:07 UTC 2019

--- test/solutions/SUM/test-sum.js
+++ test/solutions/SUM/test-sum.js
@@ -8,4 +8,20 @@ describe('SUM challenge: adding two numbers', function() {
 	it('should return 3, which is the sum of 1 and 2', function() {
 	    assert.equal(sum(1, 2), 3);
 	});
+
+	it('should throw an error when param[0] is less than 0', function() {
+		assert.throws(sum(-1,2))
+	})
+
+	it('should throw an error when param[1] is less than 0', function() {
+		assert.throws(sum(1,-2))
+	})
+
+	it('should throw an error is param[0] is greater than 100', function() {
+		asssert.throws(sum(101,2))
+	}
+
+	it('should throw an error is param[1] is greater than 100', function() {
+		asssert.throws(sum(1,102))
+	}
 });
